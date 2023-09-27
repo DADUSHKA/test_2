d2b9d0f35a15b82e9a7ed1b923245d1f110751c8
Victor <33927289+DADUSHKA@users.noreply.github.com>
2023-09-27 13:18:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function loadTable(data) {
   const tableBody = document.querySelector('tbody')
   tableBody.innerHTML = ''
 
-  // data.slice(0, 5).forEach((row) => {
   data.forEach((row) => {
     const tr = document.createElement('tr')
     tableBody.appendChild(tr)
@@ -37,18 +36,6 @@ function loadTable(data) {
       tr.appendChild(td)
     })
   })
-
-  let table = document.getElementById('table')
-
-  table.onclick = function (e) {
-    if (e.target.tagName != 'TH') return
-    let th = e.target
-    let sortDirection =
-      header.getAttribute("data-sort-direction") === "asc" ? "desc" : "asc"
-    header.setAttribute("data-sort-direction", sortDirection)
-
-    sortTable(th.cellIndex, th.dataset.type, table, sortDirection)
-  }
 }
 
 function showError(message) {
@@ -56,66 +43,6 @@ function showError(message) {
   error.className = 'error'
   error.textContent = message
   document.body.insertBefore(error, document.body.firstChild)
-}
-
-function sortTable(colNum, type, table, direction) {
-  let tbody = table.querySelector('tbody')
-  let rowsArray = Array.from(tbody.rows)
-  let compare;
-
-  switch(type) {
-    case 'number':
-      compare = function(rowA, rowB) {
-        let result;
-
-        if (direction == 'asc') {
-          result = rowB.cells[colNum].innerHTML - rowA.cells[colNum].innerHTML
-        } else {
-          result = rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML
-        }
-
-        return result
-      }
-      break;
-
-    case 'string':
-      compare = function (rowA, rowB) {
-        let result;
-
-        if (direction == 'asc') {
-           result = rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML
-        } else {
-           result = rowA.cells[colNum].innerHTML < rowB.cells[colNum].innerHTML
-        }
-
-        return result ? 1 : -1
-      }
-      break;
-  }
-
-  rowsArray.sort(compare)
-
-  tbody.append(...rowsArray)
-}
-
-function tableSearch() {
-  let phrase = document.getElementById('input').value
-  let table = document.getElementById('table')
-
-  let regPhrase = new RegExp(phrase.substring(0, phrase.length - 1), 'i')
-    let flag = false
-    for (let i = 1; i < table.rows.length; i++) {
-      flag = false
-      for (let j = table.rows[i].cells.length - 1; j >= 0; j--) {
-        flag = regPhrase.test(table.rows[i].cells[j].innerHTML)
-        if (flag) break
-      }
-      if (flag) {
-        table.rows[i].style.display = ""
-      } else {
-        table.rows[i].style.display = "none"
-      }
-    }
 }
 
 loadData('https://jsonplaceholder.typicode.com/posts', loadTable, showError);
